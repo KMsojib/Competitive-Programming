c67db96d51e2f69abd7af4502c431eeac3c48472
Kawsar <kawsarmahmud822@gmail.com>
2024-09-20 00:21:36 +0600

    One day become Day one

---
+++ Template/template.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+
+int main()
+{
+    fast_io;
+    int t=1;  ///cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
